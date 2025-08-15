222742f68c33d13bc6d26895880b5dd30640b406
Ghost171717 <ghost40031717@gmail.com>
2025-08-15 15:26:28 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'AcoModz Development'
+description 'Police Proximity Detector with UI, Notifications, and Sound (Fixed UI Command)'
+version '1.2.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+client_scripts {
+    'client.lua'
+}
